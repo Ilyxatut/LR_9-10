2a01e17c15972fb01f02f51d89bf6b2751cc478f
Ilyxatut <tutynin2007@gmail.com>
2024-12-19 08:59:50 +0300

    Add files via upload

---
+++ DZ1.pas
@@ -0,0 +1,46 @@
+﻿const N = 10;
+var arr: array[1..N]  of integer;
+    brr: array[1..N]  of integer;
+    i: integer;
+    sum1,sum2: integer;
+begin
+  sum1:= 0;
+  sum2:= 0;
+  for i:=1 to N do 
+    begin
+  arr[i] := Random(20)-10;
+  write(arr[i],' ');
+  end;
+  writeln;
+  for i:=1 to N do 
+    begin
+  brr[i] := Random(20)-10;
+  write(brr[i],' ');
+  end;
+  for i:=1 to N do begin
+    if arr[i] > 0 then
+    sum1:= sum1 + arr[i];
+    if brr[i] > 0 then
+    sum2:= sum2 + brr[i];
+  end;
+  writeln;
+  writeln;
+  if sum1 < sum2 then begin
+    for i:=1 to N do begin
+      arr[i] := arr[i] * 10;
+      write(arr[i],' ');
+    end;
+    end;
+  if sum2 < sum1 then begin
+     for i:=1 to N do begin
+      brr[i] := brr[i] * 10;
+      write(brr[i],' ');
+    end;
+    end;
+    
+  writeln;
+  writeln;
+  write(sum1);
+  writeln;
+  write(sum2);
+end.
---
+++ DZ2.pas
@@ -0,0 +1,47 @@
+﻿const
+  N = 8; 
+
+var
+  matrix: array[1..N, 1..N] of Integer;
+  minArray: array[1..N] of Integer;
+  i, j: Integer;
+  sum, average: Integer;
+  
+
+begin
+  Randomize; 
+  for i := 1 to N do
+  begin
+    for j := 1 to N do
+    begin
+      
+      matrix[i, j] := Random(100);
+      write(' ',matrix[i, j]);
+   end;
+   writeln;
+   end;
+
+  // Находим минимальные элементы и суммируем их
+  sum := 0;
+  for i := 1 to N do
+  begin
+    minArray[i] := matrix[i, 1]; 
+    for j := 2 to N do
+      if matrix[i, j] < minArray[i] then
+        minArray[i] := matrix[i, j]; 
+
+    sum := sum + minArray[i]; 
+  end;
+
+  average := sum div N; 
+  
+  WriteLn('Минимальные элементы строк:');
+  for i := 1 to N do
+    Write(minArray[i], ' ');
+
+  WriteLn;
+  
+  WriteLn('Среднее арифметическое: ', average);
+ 
+end.
+
